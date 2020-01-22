7c69d792f8b3be098d0a4a5c35bf01bc377a0851
RS-Coop <simpson.r.cooper@gmail.com>
2020-01-21 19:52:22 -0700

    Little stuff

--- nn4mc_py/parser/hdf5_parser/_hdf5parser.py
+++ nn4mc_py/parser/hdf5_parser/_hdf5parser.py
@@ -43,15 +43,28 @@ class HDF5Parser(Parser):
     #Parses all of the layer metadata
     #NOTE:
     def parseModelConfig(self):
-        with h5py.File(self.file_name, 'r') as h5file:
-            mainGroup = h5file['/']
-            configAttr = mainGroup['model_config']
+        with h5py.File(self.file_name, 'r') as h5file: #Open file
+            configAttr = h5file['/'].attrs['model_config'] #Gets all metadata
+
+            #Do something with this
 
     #Parses all of the weights
     #NOTE:
     def parseWeights(self):
-        with h5py.File(self.file_name,'r') as h5file: #Open file in readonly
-            weightGroup = h5file['model_weights'] #Open weight group.
+        with h5py.File(self.file_name,'r') as h5file: #Open file
+            weightGroup = h5file['model_weights'] #Open weight group
+
+            for id in weightGroup.keys():
+                try: #Access weights and biases
+                    #NOTE: Not a numpy array, but hdf5 dataset is similar
+                    weight = weightGroup[id][id]['kernel:0']
+                    bias = weightGroup[id][id]['bias:0']
+
+                except: #Layer type with not weights
+                    weight = None
+                    bias = None
+
+                #Do something here
 
     #
     #NOTE:
