30b7e2f26dc96d3e0c11c571d90a4f9fbbfa374d
RS-Coop <simpson.r.cooper@gmail.com>
2020-04-11 16:08:29 -0600

    Will I ever actually be ready to publish, hopefully now I am. Weight parameter stuff is fixed.

--- nn4mc_py/datastructures/layer/_weights.py
+++ nn4mc_py/datastructures/layer/_weights.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 #Class to hold weight or bias arrays for layers
 class Weight:
     def __init__(self, id, nparray=None):
@@ -14,36 +16,57 @@ class Weight:
         if self.values.any() == None:
             return ''
 
-        shape = self.values.shape
+        flat = self.values.flatten()
+        size = flat.shape[0]
 
-        if len(shape) == 1:
-            param_string = 'float ' + self.identifier +\
-                            '[' + str(shape[0]) + '] = {'
+        param_string = 'float ' + self.identifier +\
+                        '[' + str(size) + '] = {'
 
-            param_string = param_string + '{'
-            for i in range(shape[0]):
-                param_string = param_string + str(self.values[i])
+        for i in range(size):
+            param_string = param_string + str(flat[i])
 
-                if i<shape[0]-1:
-                    param_string = param_string + ', '
+            if i<size-1:
+                param_string = param_string + ', '
 
-            param_string = param_string + '}\n'
-
-        else:
-            param_string = 'float ' + self.identifier +\
-                            '[' + str(shape[0]) + '][' + str(shape[1]) + '] = {'
-
-            for i in range(shape[0]):
-                param_string = param_string + '{'
-                for j in range(shape[1]):
-                    param_string = param_string + str(self.values[i][j])
-
-                    if j<shape[1]-1:
-                        param_string = param_string + ', '
-
-                if i<shape[0]-1:
-                    param_string = param_string + '},\n\t\t\t\t\t\t\t\t\t'
-
-            param_string = param_string + '}\n'
+        param_string = param_string + '}\n\n'
 
         return param_string
+
+
+    #Deprecated
+    # def getParams(self):
+    #     if self.values.any() == None:
+    #         return ''
+    #
+    #     shape = self.values.shape
+    #
+    #     if len(shape) == 1:
+    #         param_string = 'float ' + self.identifier +\
+    #                         '[' + str(shape[0]) + '] = {'
+    #
+    #         for i in range(shape[0]):
+    #             param_string = param_string + str(self.values[i])
+    #
+    #             if i<shape[0]-1:
+    #                 param_string = param_string + ', '
+    #
+    #         param_string = param_string + '}\n'
+    #
+    #     else:
+    #         param_string = 'float ' + self.identifier +\
+    #                         '[' + str(shape[0]) + '][' + str(shape[1]) + '] = {'
+    #
+    #         for i in range(shape[0]):
+    #             param_string = param_string + '{'
+    #             for j in range(shape[1]):
+    #                 param_string = param_string + str(self.values[i][j])
+    #
+    #                 if j<shape[1]-1:
+    #                     param_string = param_string + ', '
+    #
+    #             if i<shape[0]-1:
+    #                 param_string = param_string + '},\n\t\t\t\t\t\t\t\t\t'
+    #
+    #         param_string = param_string + '}\n'
+    #
+    #     return param_string
