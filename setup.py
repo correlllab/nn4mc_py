d005dcac7fa6b34258baf3290f34a9eee0981611
RS-Coop <simpson.r.cooper@gmail.com>
2020-04-11 16:36:08 -0600

    Ready to go...

--- setup.py
+++ setup.py
@@ -5,16 +5,17 @@ with open('README.md', 'r') as ld:
 
 setup(name='nn4mc',
         version='0.1.0',
-        description='Python package for generating microcontroller code in c from'
-        'neural networks.',
+        description='Neural Networks for Microcontrollers is a Python package'
+        'for generating microcontroller code in c from traind models.',
         long_description=long_description,
         long_description_content_type='text/markdown',
         author='Cooper Simpson, Sarah Manzano, Dana Hughes',
         author_email='cooper.simpson@colorado.edu',
         url='https://github.com/RS-Coop/nn4mc_py',
+        download_url = '',
         liscense="MIT",
-        # packages=find_packages(exclude),
-        packages=["nn4mc_py"],
+        # packages=[find_packages(exclude=('tests',))],
+        packages=['nn4mc_py'],
         include_package_data=True,
         install_requires=['h5py', 'numpy'], #Might need more here
         classifiers=[
