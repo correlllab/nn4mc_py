ccdc1b4e221ad6b6d4c7ab499eefe7c2028577af
sarahaguasvivas <saag5228@colorado.edu>
2021-01-15 15:26:31 -0700

    activation unit test ready

--- tests/testing_output_files/src/activation/activation_test.py
+++ tests/testing_output_files/src/activation/activation_test.py
@@ -1,22 +1,66 @@
 import activation
-from testing_output_files.template_test import TemplateTest
+import numpy as np
+import unittest
+import tensorflow as tf
 
-class ActivationTest(TemplateTest):
-    """
-        Etash TODO
-    """
-    def __init__(self):
-        pass
+class ActivationTest(unittest.TestCase):
 
-    def test_sigmoid(self) -> bool:
-        pass
+    def test_sigmoid(self):
+        for _ in range(1000):
+            x = np.random.uniform(low = -5., high = 5.)
+            y_numpy = float(tf.keras.activations.sigmoid(x))
+            y_nn4mc = activation.sigmoid(x)
+            self.assertTrue(np.isclose(y_nn4mc, y_numpy, 1e-5))
+        print("sigmoid tested!")
 
-    """
-    [...] Test individual methods
-    """
+    def test_softplus(self):
+        for _ in range(1000):
+            x = np.random.uniform(low = -5., high = 5.)
+            y_numpy = float(tf.keras.activations.softplus(x))
+            y_nn4mc = activation.softplus(x)
+            self.assertTrue(np.isclose(y_nn4mc, y_numpy, 1e-5))
+        print("softplus tested")
 
-    def passes(self) -> bool:
-        # returns (test_sigmoid and test_hard_sigmoid and ...)
-        pass
+    def test_softsign(self):
+        for _ in range(1000):
+            x = np.random.uniform(low = -1000., high = 1000.)
+            y_numpy = float(tf.keras.activations.softsign(x))
+            y_nn4mc = activation.softsign(x)
+            self.assertTrue(np.isclose(y_nn4mc, y_numpy, 1e-5))
+        print("softsign tested")
+
+    def test_hard_sigmoid(self):
+        for _ in range(1000):
+            x = np.random.uniform(low = -1., high = 1.)
+            y_numpy = float(tf.keras.activations.hard_sigmoid(x))
+            y_nn4mc = activation.hard_sigmoid(x)
+            self.assertTrue(np.isclose(y_nn4mc, y_numpy, 1e-5))
+        print("hard sigmoid tested")
+
+    def test_relu(self):
+        for _ in range(1000):
+            x = np.random.uniform(low = -1000., high = 1000.)
+            y_numpy = float(tf.keras.activations.relu(x))
+            y_nn4mc = activation.relu(x)
+            self.assertTrue(np.isclose(y_nn4mc, y_numpy, 1e-5))
+        print("relu tested")
+
+    def test_hyper_tan(self):
+        for _ in range(1000):
+            x = np.random.uniform(low = -1000., high = 1000.)
+            y_numpy = float(tf.keras.activations.tanh(x))
+            y_nn4mc = activation.hyper_tan(x)
+            self.assertTrue(np.isclose(y_nn4mc, y_numpy, 1e-5))
+        print("tanh tested")
 
 
+    def test_softmax(self):
+        for _ in range(1000):
+            x = np.random.uniform(low = -10., high = 10., size =  10).flatten()
+            y_numpy = float(tf.keras.activations.softmax(x))
+            y_nn4mc = activation.softmax(x)
+            self.assertTrue(np.isclose(y_nn4mc, y_numpy, 1e-5))
+        print("softmax tested")
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/testing_output_files/template_test.py
+++
@@ -1,9 +0,0 @@
-from abc  import ABC, abstractmethod
-
-class TemplateTest(ABC):
-    def __init__(self):
-        pass
-
-    @abstractmethod
-    def passes(self) -> bool:
-        pass
