93ff6e9cb6877b0d80bc5761efa93a8ba848f963
bang6319 <nguyenbuithienbao@gmail.com>
2022-04-19 14:55:44 -0600

    Update: ONNX parseWeights
    Hi,
    
    Here is the def parseWeights that I made for _onnxparser.py based on one from HDF5.
    
    Bao

--- nn4mc/parser/hdf5_parser/_hdf5parser.py
+++ nn4mc/parser/hdf5_parser/_hdf5parser.py
@@ -52,10 +52,15 @@ class HDF5Parser(Parser):
 
     def onnx_parse(self, h5file):
         # Parse model configuration (i.e metadata)
+        model = keras.models.model_from_json(h5file.to_json())
+        model.set_weights(h5file.weights)
+        model.save('model.hdf5', overwrite = True)
+        h5file = h5py.File('model.hdf5', 'r')
+
         self.parseModelConfig(h5file)
 
         # Parse weights and biases
-        self.parseWeights(h5file)
+        self.parseWeights(h5file, True)
 
         # Close the file
         h5file.close()
@@ -65,10 +70,10 @@ class HDF5Parser(Parser):
     def parseModelConfig(self, h5file):
 
         # with h5py.File(self.file_name, 'r') as h5file: #Open hdf5 file
-        if not isinstance(h5file, keras.engine.functional.Functional):
-            configAttr = h5file['/'].attrs['model_config'] #Gets all metadata
-        else:
-            configAttr = h5file.to_json()
+        #if not isinstance(h5file, keras.engine.functional.Functional):
+        configAttr = h5file['/'].attrs['model_config'] #Gets all metadata
+        #else:
+        #    configAttr = h5file.to_json()
 
         configJSON = bytesToJSON(configAttr)
 
@@ -98,68 +103,44 @@ class HDF5Parser(Parser):
 
     #Parses all of the weights
     #NOTE:
-    def parseWeights(self, h5file):
-        if not isinstance(h5file, keras.engine.functional.Functional):
-            weightGroup = h5file['model_weights'] #Open weight group
-            # NOTE(sarahaguasvivas) here, the order matters,
-            #                       therefore, using different list
+    def parseWeights(self, h5file, onnx_parser = False):
+        #if not isinstance(h5file, keras.engine.functional.Functional):
+        weightGroup = h5file['model_weights'] #Open weight group
+        if (not onnx_parser):
+        #if not isinstance(h5file, keras.engine.functional.Functional):
+            for layer in self.nn.iterate_layer_list():
+                id = layer.identifier
+                if id in weightGroup.keys() and 'max_pooling1d' not in id \
+                        and 'max_pooling2d' not in id and 'flatten' not in id and \
+                        'input' not in id:
+                    # NOTE(sarahaguasvivas): kernel/weight assigment
+                    gru_keys = [k for k, v in weightGroup[id][id].items() if 'gru_cell' in k]
+                    if len(gru_keys) > 0:
+                        weight = np.array(weightGroup[id][id][gru_keys[0]]['kernel:0'])
+                    else:
+                        weight = np.array(weightGroup[id][id]['kernel:0'][()])
+                    # NOTE(sarahaguasvivas): bias
+                    if len(gru_keys) > 0:
+                        bias = np.array(weightGroup[id][id][gru_keys[0]]['bias:0'])
+                    else:
+                        bias = np.array(weightGroup[id][id]['bias:0'][()])
+                    # NOTE(sarahaguasvivas): recurrent weights
+                    if len(gru_keys) > 0:
+                        rec_weight = np.array(weightGroup[id][id][gru_keys[0]]['recurrent_kernel:0'][()])
+                    else:
+                        rec_weight = None
+                    layer.setParameters('weight', (id + '_W', weight))
+                    layer.setParameters('bias', (id + '_b', bias))
+                    layer.setParameters('weight_rec', (id + '_Wrec', rec_weight))
+
+            # NOTE(sarahaguasvivas): calculating output shapes
+            input_shape = self.nn_input_size
+            for layer in self.nn.iterate_layer_list():
+                if "input" not in layer.identifier:
+                    input_shape = layer.computeOutShape(input_shape)
+                    print(layer.getParameters())
         else:
-            h5file.save_weights('weights.h5', overwrite = True,
-                                        save_format = 'h5')
-            # TODO(sarahaguasvivas): find where the dictionary is
-            # when you read from h5py.File and assign it to
-            # weightGroup,
-
-            """
-                NOTE(sarahaguasvivas): What you want at this point is
-                for weightGroup to be a serialized dictionary with keys 
-                being the names of the layers and values being a tuple
-                of weights and biases. For example, 
-                
-                weightGroup = {'layer_name_0' : (weights_0, biases_0), 
-                               'layer_name_1' : (weights_1, biases_1 ...
-                               }
-                You can either: 
-                    1) Try to get it directly from the h5py object that we're 
-                    reading --> Running the debug session and digging into the 
-                        weightGroup object on the debugger
-                    2) Serialize it yourself, but make sure it generalizes 
-                    to any model --> digging into model configuration and 
-                        h5file.get_weights() to map the layer names to the
-                        right weights and biases
-            """
-            weightGroup = h5py.File('weights.h5', 'r')
-        for layer in self.nn.iterate_layer_list():
-            id = layer.identifier
-            if id in weightGroup.keys() and 'max_pooling1d' not in id \
-                    and 'max_pooling2d' not in id and 'flatten' not in id and \
-                    'input' not in id:
-                # NOTE(sarahaguasvivas): kernel/weight assigment
-                gru_keys = [k for k, v in weightGroup[id][id].items() if 'gru_cell' in k]
-                if len(gru_keys) > 0:
-                    weight = np.array(weightGroup[id][id][gru_keys[0]]['kernel:0'])
-                else:
-                    weight = np.array(weightGroup[id][id]['kernel:0'][()])
-                # NOTE(sarahaguasvivas): bias
-                if len(gru_keys) > 0:
-                    bias = np.array(weightGroup[id][id][gru_keys[0]]['bias:0'])
-                else:
-                    bias = np.array(weightGroup[id][id]['bias:0'][()])
-                # NOTE(sarahaguasvivas): recurrent weights
-                if len(gru_keys) > 0:
-                    rec_weight = np.array(weightGroup[id][id][gru_keys[0]]['recurrent_kernel:0'][()])
-                else:
-                    rec_weight = None
-                layer.setParameters('weight', (id + '_W', weight))
-                layer.setParameters('bias', (id + '_b', bias))
-                layer.setParameters('weight_rec', (id + '_Wrec', rec_weight))
-
-        # NOTE(sarahaguasvivas): calculating output shapes
-        input_shape = self.nn_input_size
-        for layer in self.nn.iterate_layer_list():
-            if "input" not in layer.identifier:
-                input_shape = layer.computeOutShape(input_shape)
-                print(layer.getParameters())
+            pass
 
     #parses model for input size
     def parse_nn_input(self, model_config : dict):
--- nn4mc/parser/onnx_parser/_onnxparser.py
+++ nn4mc/parser/onnx_parser/_onnxparser.py
@@ -4,7 +4,7 @@ from nn4mc.datastructures import NeuralNetwork
 # import onnx
 # from onnx2keras import onnx_to_keras
 # from nn4mc.parser.onnx_parser.onnx_helpers import HDF5Parser
-# import numpy as np
+import numpy as np
 from nn4mc.parser.onnx_parser.onnx_helpers import onnx2keras
 from nn4mc.parser.hdf5_parser._hdf5parser import HDF5Parser
 from tensorflow import keras
@@ -25,5 +25,54 @@ class ONNXParser(Parser):
         h5parser.onnx_parse(h5format)
         self.nn = h5parser.nn
 
+        # parse weights and biases
+        self.parseWeights(h5format)
+
+        # close the file
+        # h5format.close()
+
     def _parseONNX(self):
         return self.parse.h5format
+
+    def parseWeights(self, h5file, _parseONNX = False):
+
+        weightGroup = h5file['model_weights']
+
+        if (not _parseONNX):
+
+            for layer in self.nn.iterate_layer_list():
+
+                id = layer.identifier
+
+                if id in weightGroup.keys() and 'max_pooling1d' not in id \
+                        and 'max_pooling2d' not in id and 'flatten' not in id and \
+                        'input' not in id:
+
+                    gru_keys = [k for k, v in weightGroup[id][id].items() if 'gru_cell' in k]
+                    # kernel/weight assignment
+                    if len(gru_keys) > 0:
+                        weight = np.array(weightGroup[id][id][gru_keys[0]]['kernel:0'])
+                    else:
+                        weight = np.array(weightGroup[id][id]['kernel:0'][()])
+                    # bias
+                    if len(gru_keys) > 0:
+                        bias = np.array(weightGroup[id][id][gru_keys[0]]['bias:0'])
+                    else:
+                        bias = np.array(weightGroup[id][id]['bias:0'][()])
+                    # weight
+                    if len(gru_keys) > 0:
+                        rec_weight = np.array(weightGroup[id][id][gru_keys[0]]['recurrent_kernel:0'][()])
+                    else:
+                        rec_weight = None
+
+                    layer.setParameters('weight', (id + '_W', weight))
+                    layer.setParameters('bias', (id + '_b', bias))
+                    layer.setParameters('weight_rec', (id + '_Wrec', rec_weight))
+
+            input_shape = self.nn_input_size
+            for layer in self.nn.iterate_layer_list():
+                if "input" not in layer.identifier:
+                    input_shape = layer.computeOutShape(input_shape)
+                    print(layer.getParameters())
+        else:
+            pass
